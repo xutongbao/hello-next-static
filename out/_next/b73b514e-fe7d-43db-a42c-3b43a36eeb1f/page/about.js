b7dbeadce1aa318d89533639671683c4ae08a303
徐同保 <xutongbao@vackbot.com>
2018-12-04 14:42:37 +0800

    init

---
+++ out/_next/b73b514e-fe7d-43db-a42c-3b43a36eeb1f/page/about.js
@@ -0,0 +1,1 @@
+module.exports=__NEXT_REGISTER_PAGE("/about",function(){return{page:webpackJsonp([4],{197:function(e,t,n){e.exports=n(198)},198:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(5),a=n.n(u),o=n(70);t.default=function(){return a.a.createElement(o.a,null,a.a.createElement("p",null,"This is the about page"))}}},[197]).default}});
